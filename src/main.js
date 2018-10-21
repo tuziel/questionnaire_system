15aa2bed99bf96503217f794ec053531e64cf201
tuziel <tuziel@outlook.com>
2018-10-21 16:06:21 +0800

    update: 添加UI库

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+import './styles/style.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './styles/theme.css'
+
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
   router,
