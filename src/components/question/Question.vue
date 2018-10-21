43eaf1d354556d6fc7116b1ea727d959c9ffe543
tuziel <tuziel@outlook.com>
2018-10-22 00:55:56 +0800

    update: 添加问题卡组件

---
+++ src/components/question/Question.vue
@@ -0,0 +1,84 @@
+<template>
+  <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <span>{{title}}</span>
+    </div>
+
+    <div v-if="isSingleQuest">
+      <div class="options" v-for="option in options" :key="option.id">
+        <el-radio v-model="radio" :label="option.text" @change="updateResult">{{option.text}}</el-radio>
+      </div>
+    </div>
+
+    <div v-else-if="isMultipleQuest">
+      <el-checkbox-group v-model="checkList" @change="updateResult">
+        <div class="options" v-for="option in options" :key="option.id">
+          <el-checkbox :label="option.text"></el-checkbox>
+        </div>
+      </el-checkbox-group>
+    </div>
+
+    <div v-if="isTextQuest">
+      <el-input type="textarea" :autosize="{ minRows: 5 }" v-model="textarea" placeholder="请输入内容" @change="updateResult"></el-input>
+    </div>
+  </el-card>
+</template>
+
+<script>
+const TYPE_SINGLE_CHOICE = 1
+const TYPE_MULTIPLE_CHOICE = 2
+const TYPE_TEXT = 3
+
+export default {
+  name: 'question',
+  props: ['quest'],
+  data () {
+    return {
+      ...this.quest,
+      radio: '',
+      checkList: [],
+      textarea: ''
+    }
+  },
+  methods: {
+    updateResult (result) {
+      this.$emit('change', result)
+    }
+  },
+  computed: {
+    isSingleQuest: function () {
+      return this.type === TYPE_SINGLE_CHOICE
+    },
+    isMultipleQuest: function () {
+      return this.type === TYPE_MULTIPLE_CHOICE
+    },
+    isTextQuest: function () {
+      return this.type === TYPE_TEXT
+    }
+  }
+}
+</script>
+
+<style scoped>
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
+
+.options {
+  line-height: 2;
+}
+</style>
---
+++ src/components/question/QuestionEditor.vue
@@ -0,0 +1,120 @@
+<template>
+  <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <h4 class="header-title">{{getTypeText}}</h4>
+      <div class="flex">
+        <el-input v-model="value.title" placeholder="请输入问题"></el-input>
+        <div class="contorls" v-if="isSingleQuest || isMultipleQuest">
+          <el-button type="primary" icon="el-icon-plus" circle @click="addoption"></el-button>
+        </div>
+      </div>
+    </div>
+
+    <div v-if="isSingleQuest || isMultipleQuest">
+      <div class="inputBox" v-for="(option, index) in options" :key="option.id">
+        <el-input v-model="option.text" placeholder="请输入答案"></el-input>
+        <div class="contorls">
+          <el-button type="primary" icon="el-icon-arrow-up" circle @click="putUp(index)" :disabled="index === 0"></el-button>
+          <!-- <el-button type="primary" icon="el-icon-arrow-down" circle @click="putDown(index)" :disabled="index === options.length - 1"></el-button> -->
+          <el-button type="danger" icon="el-icon-minus" circle @click="removeoption(index)"></el-button>
+        </div>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script>
+const TYPE_SINGLE_CHOICE = 1
+const TYPE_MULTIPLE_CHOICE = 2
+const TYPE_TEXT = 3
+
+export default {
+  name: 'question',
+  props: ['value'],
+  data () {
+    return {
+      ...this.value
+    }
+  },
+  methods: {
+    addoption () {
+      this.options.push({
+        text: ''
+      })
+    },
+
+    putUp (index) {
+      const options = this.options
+      const current = this.options.splice(index, 1)[0]
+      options.splice(index - 1, 0, current)
+    },
+
+    putDown (index) {
+      const options = this.options
+      const current = options.splice(index, 1)[0]
+      options.splice(index + 1, 0, current)
+    },
+
+    removeoption (index) {
+      const options = this.options
+      options.splice(index, 1)
+    }
+  },
+  computed: {
+    getTypeText: function () {
+      switch (this.type) {
+        case TYPE_SINGLE_CHOICE: {
+          return '单选题'
+        }
+        case TYPE_MULTIPLE_CHOICE: {
+          return '多选题'
+        }
+        case TYPE_TEXT: {
+          return '文本题'
+        }
+      }
+
+      return this.type
+    },
+    isSingleQuest: function () {
+      return this.type === TYPE_SINGLE_CHOICE
+    },
+    isMultipleQuest: function () {
+      return this.type === TYPE_MULTIPLE_CHOICE
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header-title {
+  margin-bottom: 18px;
+  line-height: 1;
+}
+
+.flex {
+  display: flex;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.inputBox {
+  display: flex;
+  margin-bottom: 20px;
+}
+.option {
+  display: flex;
+  flex: auto;
+}
+.contorls {
+  padding-left: 15px;
+  white-space: nowrap;
+}
+</style>
