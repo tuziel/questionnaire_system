22e99f4dfbc26fa8a296dc5e9b684e6c30bb71e2
tuziel <tuziel@outlook.com>
2018-10-21 16:29:27 +0800

    update: 添加头部导航栏

---
+++ src/views/Nav/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-menu default-active="/" :router="true" mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+    <el-menu-item v-for="(item, index) in nav" :key="index" :index="item.index">{{item.name}}</el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      nav: [{
+        index: '/',
+        name: '主页'
+      }, {
+        index: '/about',
+        name: 'about'
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
